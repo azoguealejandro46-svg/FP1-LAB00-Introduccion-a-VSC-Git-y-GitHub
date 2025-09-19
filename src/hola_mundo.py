4ad63506e06bf7f62519ace26782cf870c863996
Alejandro <aleazojim@alum.us.es>
2025-09-19 16:47:23 +0200

    Ahora tiene saludo personalizado con la hora

--- src/hola_mundo.py
+++ src/hola_mundo.py
@@ -1,3 +1,10 @@
-print("¡Hola mundo!")
+from datetime import datetime
 
-
+hora_actual = datetime.now().hour
+NOMBRE=input("Escribe tu nombre")
+if(hora_actual<12):
+    print(f"¡Buenos días, {NOMBRE}!")
+if(12<=hora_actual<20):
+    print(f"¡Buenas tardes, {NOMBRE}!")
+if(20<=hora_actual<23):
+    print(f"¡Buenas noches, {NOMBRE}!")
